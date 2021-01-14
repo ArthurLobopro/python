c1a9254540e56c5d8cc8cd476b7a948e806640a1
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-01-14 10:04:47 -0300

    Create color.py

---
+++ my_modules/color.py
@@ -0,0 +1,50 @@
+@@ -1,49 +0,0 @@
+-colors = {
+-    'black':         30,
+-    'red':           31,
+-    'green':         32,
+-    'yellow':        33,
+-    'blue':          34,
+-    'magenta':       35,
+-    'cyan':          36,
+-    'gray':          37,
+-    'dark gray':     90,
+-    'ligth red':     91,
+-    'ligth green':   92,
+-    'ligth yellow':  93,
+-    'ligth blue':    94,
+-    'ligth magenta': 95,
+-    'ligth cyan':    96,
+-    'white':         97
+-}
+-styles = {
+-    'bold':      1,
+-    'underline': 4,
+-    'negative':  7
+-}
+-
+-def cprint(string, c='', bgc='', style=0,end='\n'):
+-    end = '\033[m'+end
+-    if c != '':
+-        c= colors[c]
+-    if bgc!= '':
+-        bgc= colors[bgc]+10
+-    if style != 0:
+-        style = styles[style]
+-    if bgc=='':
+-        print ('\033[{};{}m{}'.format(style, c, string), end=end)
+-    else:
+-        print ('\033[{};{};{}m{}'.format(style, c, bgc, string), end=end)
+-
+-def creturn (string, c='', bgc='', style=0,end='\033[m'):
+-    if c != '':
+-        c= colors[c]
+-    if bgc!= '':
+-        bgc= colors[bgc]+10
+-    if style != 0:
+-        style = styles[style]
+-    if bgc=='':
+-        string_return = '\033[{};{}m{}{}'.format(style, c, string,end)
+-    else:
+-        string_return = '\033[{};{};{}m{}{}'.format(style, c, bgc, string,end)
+-    return string_return
